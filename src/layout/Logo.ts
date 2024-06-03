7e55968b346fc593b741ecf57271bd145ce21855
ketao4869 <ketao4869@gmail.com>
2024-06-03 17:57:06 +0800

    format(Layout): 优化自动创建路由处理方式
    1、调整ts类型文件到/types/layout.d.ts
    2、抽离出buildRoutes，调整路由生成逻辑。默认/src/views为路由根文件夹
    2.1、有page.ts,index.vue文件表示当前文件夹为菜单目录。如：views/api/page.ts、index.vue
    2.2、缺少page.ts表示普通目录。如views/login/index.vue
    2.3、缺少index.vue文件但是有同级子菜单目录，表示为目录节点，没有就不会渲染菜单路由。
    3、调整Menu.ts菜单渲染逻辑
    3.1、只有一个子菜单时，只显示子菜单
    3.2、若page.ts没有配置，则菜单名称为文件名称，菜单icon为ep:folder、ep:document

--- src/layout/Logo.ts
+++ src/layout/Logo.ts
@@ -1,4 +1,4 @@
-import type { logoMode } from './type'
+import type { LayoutMode } from './type'
 import { LayoutStore } from '@/stores'
 
 import LogoSrc from '@/assets/logo.svg'
@@ -10,7 +10,7 @@ export default defineComponent({
     name: 'Logo',
     props: {
         mode: {
-            type: String as PropType<logoMode>,
+            type: String as PropType<Exclude<LayoutMode, 'mix'>>,
             required: true,
         },
     },
@@ -18,16 +18,16 @@ export default defineComponent({
         const isVertical = computed(() => mode === 'vertical')
         const isHorizontal = computed(() => mode === 'horizontal')
 
-        const src = import.meta.env.VITE_LOGIN_URL || LogoSrc // 项目logo地址
+        const src = import.meta.env.VITE_LOGO_URL || LogoSrc // 项目logo地址
 
         return () => {
             return h(
                 'div',
                 {
                     class: [
-                        'h-60 flex justify-center items-center flex-shrink-0',
+                        'h-60 flex justify-between items-center flex-shrink-0',
                         isVertical.value && 'w-249 border-b-1 border-b-solid border-b-[var(--el-border-color)]',
-                        isHorizontal.value && 'w-250 border-r-1 border-r-solid border-r-[var(--el-border-color)]',
+                        isHorizontal.value && 'w-201 border-r-1 border-r-solid border-r-[var(--el-border-color)]',
                         isVertical.value && isCollapse.value && 'w-64',
                     ],
                 },
@@ -43,11 +43,12 @@ export default defineComponent({
                             h('img', {
                                 class: 'w-200',
                                 src,
-                                title: 'VITE_APP_TITLE',
+                                title: import.meta.env.VITE_APP_NAME,
                             }),
                         ],
                     ),
-                    isVertical.value && h(
+                    isVertical.value
+                    && h(
                         ElIcon,
                         {
                             class: 'flex-1 cursor-pointer',
