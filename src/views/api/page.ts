61f1c5b2d7c3340f191314b0da1f91209edd4fef
ketao4869 <54937496+ketao4869@users.noreply.github.com>
2024-06-04 09:56:12 +0800

    Revert "chore: 添加 axios、rollup-plugin-visualizer、vite-plugin-compression、vite-plugin-vue-devtools 的依赖项并创建api调试页面"
    This reverts commit 4d86135dfeb5e33789e4e8122bb2597899f0526a.

--- src/views/api/page.ts
+++
@@ -1,9 +0,0 @@
-const routeConfig: RouteConfig = {
-    meta: {
-        title: 'API 列表',
-        icon: 'i-ant-design:api-twotone',
-        menuOrder: -21,
-    },
-}
-
-export default routeConfig
